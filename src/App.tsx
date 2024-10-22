54a7a86e996492c8e42be4e6c0f69e2cdcbffd2f
Wellington Adam <wellington.adam@soraschools.com>
2024-10-22 17:16:09 -0300

    feat: new create events page

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import HomePage from 'src/pages/home'
+import CreateEventPage from './pages/events/create'
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/events/new" element={<CreateEventPage />} />
       </Routes>
     </Router>
   )
---
+++ src/pages/events/create.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import { Card } from 'src/components/molecules'
+import { Layout } from 'src/components/organisms'
+import { FormData } from 'src/components/templates'
+import { createEvent } from 'src/api/eventsApi'
+import { Event, EventRequest } from 'src/types/eventType'
+
+const CreateEventPage = () => {
+  const [loading, setLoading] = useState(false)
+  const postCreateNewEvent = async (event: EventRequest, image?: File) => {
+    try {
+      setLoading(true)
+      await createEvent(event, image)
+      //TODO: Add success message
+    } catch (error) {
+      console.log('error getAllEvents', error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  return (
+    <Layout>
+      <div className="flex flex-col gap-8 py-10">
+        <h1>Create new Event</h1>
+        <FormData
+          submitButtonLabel="Create new Event"
+          onSubmitForm={(data, image) => postCreateNewEvent(data, image)}
+          loadingSubmit={loading}
+        />
+      </div>
+    </Layout>
+  )
+}
+
+export default CreateEventPage
